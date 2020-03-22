00624f245625b0f6f99c80e7c046ad762785c777
rafallk <62295227+rafallk@users.noreply.github.com>
2020-03-22 02:20:50 +0100

    Update LoadData.py

--- Covid19/LoadData.py
+++ Covid19/LoadData.py
@@ -10,7 +10,7 @@ print(workDir)
 confirmed = open(f"{workDir}time_series_19-covid-Confirmed.csv", "r")
 deaths = open(f"{workDir}time_series_19-covid-Deaths.csv", "r")
 recovered = open(f"{workDir}time_series_19-covid-Recovered.csv", "r")
-files = [confirmed.readlines(), deaths.readlines(), recovered.readlines()]
+files = [confirmed.read().split("\n"), deaths.read().split("\n"), recovered.read().split("\n")]
 
 europeList = open("Definitions\\europe.txt").read().split("\n")
 
@@ -43,6 +43,7 @@ class Area:
 
     def __init__(self, _name, head, _confirmed, _deaths, _recovered):
         self.name = f"{_name[0]}, {_name[1]}" if _name[0] != "" else _name[1]
+        self.name = _name if isinstance(_name, str) else self.name
         self.date = head[4:]
         self.latitude = head[2]
         self.longitude = head[3]
@@ -53,14 +54,13 @@ class Area:
         self.attributes = []
 
 
-
     def calcActive(self):
-        active = []
+        active = {}
         for i in range(len(self.confirmed)):
-            _confirmed = self.confirmed[i]
-            _deaths = self.deaths[i]
-            _recovered = self.recovered[i]
-            active.append(_confirmed - _deaths - _recovered)
+            _confirmed = self.confirmed[self.date[1]]
+            _deaths = self.deaths[self.date[1]]
+            _recovered = self.recovered[self.date[1]]
+            active[self.date[i]] = (_confirmed - _deaths - _recovered)
         return active
 
     def MarkAsEurope(self, _area, _list, _marker):
@@ -74,26 +74,32 @@ class Covid:
         self.areas = []
         self.filtered = []
         self.count = len(inputData[0])-1
+        _head = inputData[0][0]
+        print(f"HEAD:: {_head}")
+        self.head = _head
         for i in range(self.count):
             index = i+1
-            _head = inputData[0][1]
-            self.head = _head
+
             _name = inputData[0][index][:2]
-            _confirmed = self.toIntData(inputData[0][index][4:])
-            _deaths = self.toIntData(inputData[1][index][4:])
-            _recovered = self.toIntData(inputData[2][index][4:])
+            _confirmed = self.toIntData(_head[4:], inputData[0][index][4:])
+            _deaths = self.toIntData(_head[4:], inputData[1][index][4:])
+            _recovered = self.toIntData(_head[4:], inputData[2][index][4:])
             area = Area(_name, _head, _confirmed, _deaths, _recovered)
 
             area.MarkAsEurope(area.name, europeList, self.europe)
             self.areas.append(area)
 
-    def toIntData(self, _data):
-        result = []
-        for i in range(len(_data)):
+    def toIntData(self, date, _data):
+        result = {}
+        print(f"date len: {len(date)}")
+        print(f"data len: {len(_data)}")
+        y = len(_data) - len(date)
+        for i in range(len(_data)-y):
             try:
-                result.append(int(_data[i]))
+                result[date[i]] = (int(_data[i]))
             except:
-                result.append(int(_data[i].rstrip()))
+                result[date[i]] = (int(_data[i+y].rstrip()))
+                pass
         return result
 
     def printList(self):
@@ -101,11 +107,12 @@ class Covid:
 
     def _list(self, elements):
         for element in elements:
+            key = element.date[-1]
             print(f"{element.name}:: "
-                  f"confirmed: {element.confirmed[-1]}, "
-                  f"deaths: {element.deaths[-1]}, "
-                  f"recovered: {element.recovered[-1]}, "
-                  f"active: {element.active[-1]}" + ", " + self.europe if self.europe in element.attributes else ""
+                  f"confirmed: {element.confirmed[key]}, "
+                  f"deaths: {element.deaths[key]}, "
+                  f"recovered: {element.recovered[key]}, "
+                  f"active: {element.active[key]}" + ", " + self.europe if self.europe in element.attributes else ""
                   )
 
     def getArea(self, name):
@@ -124,16 +131,17 @@ class Covid:
                 print(country.name)
 
     def createEurope(self):
-        _len = len(self.areas[0].confirmed)
-        _confirmed = [0] * _len
-        _deaths = [0] * _len
-        _recovered = [0] * _len
+        _date = self.areas[0].date
+        _len = len(_date)
+        confirmed_buf = {k: 0 for k in _date}
+        deaths_buf = {k: 0 for k in _date}
+        recovered_buf = {k: 0 for k in _date}
         for i in range(self.count):
             if self.europe in self.areas[i].attributes:
-                _confirmed = [x + y for x, y in zip(_confirmed, self.areas[i].confirmed)]
-                _deaths = [x + y for x, y in zip(_deaths, self.areas[i].deaths)]
-                _recovered = [x + y for x, y in zip(_recovered, self.areas[i].recovered)]
-        area = Area(self.europe, self.head, _confirmed, _deaths, _recovered)
+                confirmed_buf = {key: (confirmed_buf[key] + self.areas[i].confirmed[key]) for key in _date}
+                deaths_buf = {key: (deaths_buf[key] + self.areas[i].deaths[key]) for key in _date}
+                recovered_buf = {key: (recovered_buf[key] + self.areas[i].recovered[key]) for key in _date}
+        area = Area(self.europe, self.head, confirmed_buf, deaths_buf, recovered_buf)
         area.attributes.append("Continent")
         self._list([area])
         self.areas.append(area)
@@ -143,15 +151,26 @@ class Covid:
         result = list(filter(lambda x: x.europe in x.attributes, self.areas))
         self._list(result)
 
-
+a = ["a", "b", "c"]
+b = ["1", "2", "3"]
+d = ["5", "6", "7"]
+c = {k: v1+v2 for (k, v1, v2) in zip(a, b, d)}
+e = {k: v for (k, v) in zip(a, [0]*len(a))}
+e = {k: 0 for k in a}
+print(f"test:: {c}")
+print(f"test:: {e}")
 
 w = Covid(data)
 # w.printList()
 
 #print(w.list())
-country = w.getArea("Italy")[0]
+country = w.getArea("Germany")[0]
 print(country.name + ": " + str(country.confirmed))
 w.createEurope()
+aa = w.getArea("Europe")[0]
+print(aa.name + ": " + str(aa.confirmed))
+
+
 # area = "United Kingdom"
 # print(area in europeList)
 # print(area in europeList or f"{area}, {area}" in europeList)
